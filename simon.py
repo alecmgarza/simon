af942119ddc761ea432df7b2decd7af973fd500b
Alec Garza <alecmgarza@gmail.com>
2024-02-29 23:32:12 -0600

    Simon's picks now disappear after each number is selected

--- simon.py
+++ simon.py
@@ -6,12 +6,19 @@ import os
 def game():
     first_pick = 1
     last_pick = 3
+
+    os.system('cls')
     
     def simon_says(first_pick, last_pick):
         sequence = ''
+        print('Memorize the following sequence: \n')
+        time.sleep(3)
+        os.system('cls')
         while first_pick <= last_pick:
             pick = random.choice('0123456789')
             print(pick)
+            time.sleep(1)
+            os.system('cls')
             sequence += pick
             first_pick += 1
             time.sleep(1)
@@ -22,6 +29,8 @@ def game():
 
         if player == sequence:
             print('Good job\n')
+            time.sleep(2)
+            os.system('cls')
             first_pick = 1
             last_pick += 1
             simon_says(first_pick, last_pick)
