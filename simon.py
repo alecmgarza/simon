8c1e21d13dad501459d28b77e1f60fa2a2cf4811
Alec Garza <alecmgarza@gmail.com>
2024-02-29 21:42:55 -0600

    Player can enter the given sequence, condition checks to see if input matches the sequence

--- simon.py
+++ simon.py
@@ -5,7 +5,18 @@ first_pick = 1
 
 last_pick = 3
 
+sequence = ''
+
 while first_pick <= last_pick:
-    print(random.choice('0123456789'))
+    pick = random.choice('0123456789')
+    print(pick)
+    sequence += pick
     first_pick += 1
     time.sleep(1)
+
+player = input('Enter the sequence: ')
+
+if player == sequence:
+    print('Good job')
+else:
+    print('Game over')
