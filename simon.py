87e9545dd00892603ece6c42f1186e5608787d45
Alec Garza <alecmgarza@gmail.com>
2024-02-29 22:55:23 -0600

    Simon adds an additional digit for every turn the player gets correct

--- simon.py
+++ simon.py
@@ -2,24 +2,33 @@ import time
 import random
 import os
 
-first_pick = 1
 
-last_pick = 3
+def game():
+    first_pick = 1
+    last_pick = 3
+    
+    def simon_says(first_pick, last_pick):
+        sequence = ''
+        while first_pick <= last_pick:
+            pick = random.choice('0123456789')
+            print(pick)
+            sequence += pick
+            first_pick += 1
+            time.sleep(1)
 
-sequence = ''
+        os.system('cls')
 
-while first_pick <= last_pick:
-    pick = random.choice('0123456789')
-    print(pick)
-    sequence += pick
-    first_pick += 1
-    time.sleep(1)
+        player = input('Enter the sequence: ')
 
-os.system('cls')
-
-player = input('Enter the sequence: ')
-
-if player == sequence:
-    print('Good job')
-else:
-    print('Game over')
+        if player == sequence:
+            print('Good job\n')
+            first_pick = 1
+            last_pick += 1
+            simon_says(first_pick, last_pick)
+        else:
+            print('Game over')
+            first_pick = 1
+            last_pick = 3
+    
+    simon_says(first_pick, last_pick)
+game()
